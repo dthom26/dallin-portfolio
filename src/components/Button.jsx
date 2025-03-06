72cada4f092497951b2fa53e245ae04b5deb62ec
Dallin Thomson <dallin.thomson@gmail.com>
2025-03-05 18:54:37 -0600

    changed  button component color and text color.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ href, title }) {
   return (
     <button
-      className="bg-gradient-to-br from-sky-400 via-indigo-600 to-blue-900 text-white font-semibold
+      className="bg-gradient-to-br from-slate-100 via-sky-100 to-slate-300 text-black font-semibold
     px-6 py-3 rounded-xl
     border border-sky-200/30
     shadow-md shadow-indigo-900/50
