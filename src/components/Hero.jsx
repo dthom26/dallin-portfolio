6af17631b5fd88af0408413c3e47ba2821324c80
Dallin Thomson <dallin.thomson@gmail.com>
2025-03-11 13:40:08 -0500

    testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ export const Hero = () => {
         className="mx-auto mb-8 w-48 h-48 rounded-full object-cover object-top transform  transition-transform duration-300 hover:scale-105"
       /> */}
       <h1 className="text-4xl font-semibold text-center text-white font-sora">
-        Dallin Thomson Front-End Developer
+        Dallin Thomson, Front-End Developer
       </h1>
       <p className=" mt-4 text-lg text-gray-300">
         I create modern, responsive web apps for seamless, user-friendly
